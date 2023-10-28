43e4e298e0a1e65969a581cc8aa03dcf5e2ced27
Aditya Londhe <114740433+AdityaSLondhe@users.noreply.github.com>
2023-10-28 23:38:07 +0530

    Add files via upload

--- Extension/public/add.js
+++ Extension/public/add.js
@@ -1,8 +1,6 @@
-const cpubtn2 = document.getElementById('copypassword');
+const cpubtn2 = document.getElementById('addpassword');
 const conf2 =document.getElementById('confirm');
-const pwd = document.getElementById('userPassword');
 cpubtn2.addEventListener('click',()=>{
-    navigator.clipboard.writeText(pwd.value);
     conf2.classList.add('active');
     setTimeout(()=>{
         conf2.classList.remove('active');
