500d32233ac9b6128275b65ed871c8efb12bc9d7
Akshay <akshaympacad@gmail.com>
2023-10-31 03:52:21 +0530

    some files

---
+++ Extension/public/content.js
@@ -0,0 +1,24 @@
+// List of URLs you want to match against
+var urlsToMatch;
+const token = localStorage.getItem('token');
+
+fetch('http://localhost:3000/weburls', {
+    method: 'GET',
+    headers: {
+        'authorization': token,
+    }
+})
+    .then(response => response.json())
+    .then(response => {
+        urlsToMatch = response.websites;
+    })
+    .catch(err => {
+        console.error(err);
+    })
+  
+const currentURL = window.location.href;
+
+if (urlsToMatch.includes(currentURL)) {
+    chrome.runtime.sendMessage({ showPopup: true, url: currentURL });
+}
+  
